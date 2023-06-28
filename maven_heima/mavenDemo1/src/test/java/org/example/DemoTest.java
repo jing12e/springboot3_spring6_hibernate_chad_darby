6c9fd43fa1df023ead96bfe7211b6525014f074f
Jing Chen <jingc4853@gmail.com>
2023-06-28 23:53:37 +0800

    Update exercises of Chapter 1: Spring Boot Quick Start

---
+++ maven_heima/mavenDemo1/src/test/java/org/example/DemoTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import ch.qos.logback.classic.Logger;
+import org.junit.Test;
+
+public class DemoTest {
+
+    @Test
+    public void Test1() {
+        System.out.println("hello test1");
+    }
+    @Test
+    public void Test2() {
+        System.out.println("hello test2");
+    }
+    @Test
+    public void Test3() {
+        System.out.println("hello test3");
+    }
+    @Test
+    public void Test4() {
+        System.out.println("hello test4");
+    }
+}
